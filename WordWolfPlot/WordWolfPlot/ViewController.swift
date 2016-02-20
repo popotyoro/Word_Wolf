bcc0a3e7ddc539160baa79b491c0d08a7d785a79
popota <popota@popota-no-MacBook-Pro.local>
2016-02-20 17:02:32 +0900

    add sample

---
+++ WordWolfPlot/WordWolfPlot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WordWolfPlot
+//
+//  Created by popota on 2016/02/06.
+//  Copyright © 2016年 popota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
